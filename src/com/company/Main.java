e55a7e034975cb213ee3e6c8df329466d5c0b9f7
chinmayeepanigrahy <55028727+chinmayeepanigrahy@users.noreply.github.com>
2020-05-05 18:12:18 +0200

    hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("//input[@name='q']")).sendKeys("selenium");
+    }
+}
